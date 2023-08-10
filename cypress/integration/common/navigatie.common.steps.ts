cb74c2bbfa128587c3e639ca69156baf73919995
Jesper Borst <jesper.borst@topicus.nl>
2023-08-10 10:26:12 +0200

    Removal Design Spreekuur

--- cypress/integration/common/navigatie.common.steps.ts
+++ cypress/integration/common/navigatie.common.steps.ts
@@ -4,11 +4,6 @@ Given('Gebruiker navigeert naar de {string} pagina', (pagina: string) => {
     cy.visit(pagina);
 });
 
-Given('Gebruiker klikt op de Volgende knop bij de {string} vraag', (vraagTitel: string) => {
-    cy.contains(vraagTitel).should("be.visible");
-    cy.contains('button', 'volgende', { matchCase: false }).click();
-});
-
 Given('Gebruiker klikt op de Terug knop bij Digidok', () => {
     cy.contains('button', 'TERUG').click();
 });
